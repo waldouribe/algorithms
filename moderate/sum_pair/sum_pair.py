c5c73093628d89745595574a00ad17498367d279
Waldo Uribe <wuribe@gmail.com>
2015-06-20 21:07:45 -0300

    sum_pair

--- moderate/sum_pair/sum_pair.py
+++ moderate/sum_pair/sum_pair.py
@@ -1,5 +1,22 @@
 import math
 
-def sum_pair(array, sum):
-    return []
-    
+def sum_pair(array, wanted_sum):
+    sorted_array = sorted(array)
+    size = len(array)
+    pairs = []
+    start = 0
+    end = size - 1
+    while start < end:
+        start_value = sorted_array[start]
+        end_value = sorted_array[end]
+        pair_sum =  start_value + end_value
+        if pair_sum == wanted_sum:
+            pairs.append([start_value, end_value])
+            start += 1
+            end -= 1
+        elif pair_sum > wanted_sum:
+            end -= 1
+        else:
+            start += 1
+    return pairs
+ 
--- moderate/sum_pair/test_sum_pair.py
+++ moderate/sum_pair/test_sum_pair.py
@@ -1,15 +1,33 @@
 import unittest
-from my_max import *
+from sum_pair import *
 
 class TestSumPair(unittest.TestCase):
     def test_no_pair(self):
         # Arrange
         array = [1, 2, 3, 4]
-        sum = [10]
+        sum = 10
         # Act
         answer = sum_pair(array, sum)
         # Assert
         self.assertEquals(answer, [])
+
+    def test_one_pair(self):
+        # Arrange
+        array = [10, 2, 3, 1]
+        sum = 5
+        # Act
+        answer = sum_pair(array, sum)
+        # Assert
+        self.assertEquals(answer, [[2, 3]])
+
+    def test_two_pairs(self):
+        # Arrange
+        array = [4, 3, 1, 2, 5, 6]
+        sum = 5
+        # Act
+        answer = sum_pair(array, sum)
+        # Assert
+        self.assertEquals(answer, [[1, 4], [2, 3]])
 
 if __name__ == '__main__':
     unittest.main()
